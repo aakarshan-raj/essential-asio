f03d6f25705514c30fc02de52b9bffa46cd30842
aakarshan-raj <aakarshan7raj@gmail.com>
2023-03-28 21:05:37 +0530

    first asio

--- start.cpp
+++ start.cpp
@@ -1,1 +1,12 @@
 #include <iostream>
+#include <asio.hpp>  //header based lib
+
+int main(){
+
+
+	asio::io_context io;
+	asio::steady_timer t(io,asio::chrono::seconds(5));
+	t.wait();
+	std::cout<<"Wait is over!"<<std::endl;
+
+}
